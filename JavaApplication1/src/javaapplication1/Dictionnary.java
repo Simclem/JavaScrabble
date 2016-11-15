850d6a710b34ca94ee8513a024c64a9b3976894c
Clément Simon <Clment Simon>
2016-11-15 20:02:39 +0100

    Exercice 6 fini

--- JavaApplication1/src/javaapplication1/Dictionnary.java
+++ JavaApplication1/src/javaapplication1/Dictionnary.java
@@ -9,19 +9,23 @@ package javaapplication1;
  *
  * @author Clément
  */
-public class Dictionnary {
-    String[] Dico;
+public class Dictionnary
+{
+        String[] Dico;
 
-    
-    public Dictionnary()
-    {
-        Dico = new String[12];
-        Dico[0] = "abricot";
-        Dico[1] = "châtaigne";
-        Dico[2] = "groseille";
-        Dico[3] = "pomme";
-        Dico[3] = "tomate";
-        System.out.print(Dico[0] + "\n");
-    }
-        
+
+        public Dictionnary()
+        {
+            Dico = new String[12];
+            Dico[0] = "abricot";
+            Dico[1] = "châtaigne";
+            Dico[2] = "groseille";
+            Dico[3] = "pomme";
+            Dico[3] = "tomate";
+            System.out.print(Dico[0] + "\n");
+        }
+            public String[] getWordList()
+            {
+                    return Dico;
+            }
 }
--- JavaApplication1/src/javaapplication1/ScrabbleConsole.java
+++ JavaApplication1/src/javaapplication1/ScrabbleConsole.java
@@ -1,26 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package javaapplication1;
-import javaapplication1.Dictionnary;
+import java.util.Arrays;
+
 /**
  *
  * @author Clément
  */
-public class ScrabbleConsole {
+public class ScrabbleConsole
+{
+    
+        /**
+         * @param args the command line arguments
+         */
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    
-    
-    
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Scrabble assistant");
-        Dictionnary dico = new Dictionnary();
-    }
+        public ScrabbleConsole()
+        {
+                System.out.println("Welcome to the Scrabble assistant");
+                Dictionnary dico = new Dictionnary();
+                System.out.println(Arrays.toString(dico.getWordList()));
+        }
+
+        public static void main(String[] args)
+        {
+                ScrabbleConsole sc = new ScrabbleConsole();
+        }
     
 }
