5db638a6d643fea3a301fee923ffe46448ddbe15
Clément Simon <Clment Simon>
2016-11-15 23:12:30 +0100

    Ajout des fonctionnalitées de l'exo 9. Pas de Scanner, trop chiant à utiliser. ReadAllLines à la place

--- JavaApplication1/src/javaapplication1/Dictionnary.java
+++ JavaApplication1/src/javaapplication1/Dictionnary.java
@@ -4,7 +4,15 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Paths;
+import java.util.List;
 /**
  *
  * @author Clément
@@ -16,7 +24,7 @@ public class Dictionnary
 
         public Dictionnary()
         {
-            int nbWord = 5; 
+            nbWord = 12; 
             Dico = new String[nbWord];
             Dico[0] = "abricot";
             Dico[1] = "châtaigne";
@@ -25,9 +33,48 @@ public class Dictionnary
             Dico[4] = "tomate";
 
         }
+        public Dictionnary(String PathGiven)
+        {
+                int i = 0;
+                List<String> Lines = null;
+                Path pathFile = Paths.get(PathGiven);
+                try
+                {
+                    Lines = Files.readAllLines(pathFile);
+
+                }catch (IOException  e)
+                {
+                    System.out.println("Fichier introuvable");
+
+                 }
+                
+                
+                   nbWord = Integer.parseInt(Lines.get(0));
+                    Dico = new String[nbWord];
+                    
+                    for (i = 0; i <nbWord ; i++)
+                    {
+                        Dico[i] = Lines.get(i+1);
+                    }
+        }
+        
+        
+        
             public String[] getWordList()
             {
+                    int i  = 0 ;
+
                     return Dico;
+            }
+            
+            
+            public void printDictionnary()
+            {
+                    int i ;
+                     for (i = 0 ; i < nbWord; i++)
+                    {
+                        System.out.println(Dico[i] + "\n");
+                    }
             }
             
             
@@ -36,7 +83,7 @@ public class Dictionnary
             {
                    int i;
                    boolean found = false;
-                   for (i = 0 ; i  <= nbWord    ; i ++)
+                   for (i = 0 ; i  < nbWord    ; i ++)
                    {
                        
                        if (Dico[i].equals(str))
--- JavaApplication1/src/javaapplication1/ScrabbleConsole.java
+++ JavaApplication1/src/javaapplication1/ScrabbleConsole.java
@@ -21,7 +21,8 @@ public class ScrabbleConsole
         public ScrabbleConsole()
         {
                 System.out.println("Welcome to the Scrabble assistant");
-                Dictionnary dico = new Dictionnary();
+                //Dictionnary dico = new Dictionnary();
+                Dictionnary dico = new Dictionnary("./fr_FR_utf8.dico");
                 System.out.println(Arrays.toString(dico.getWordList()));
 
                 System.out.println(dico.isValidWord("test"));
